04f1cb05e7e3024deec678243e893732ddfd7c49
ImThanOs36 <lads42279@gmail.com>
2024-06-08 09:24:16 +0530

    project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <p className="text-primary-100 text-xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
