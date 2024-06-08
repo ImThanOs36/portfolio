28c25e0e776009ce22b418f1eea08143120bfc9d
ImThanOs36 <lads42279@gmail.com>
2024-06-08 12:23:30 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
-
+<Navbar/>
       <p className="text-primary-100 text-xl">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-screen h-6 bg-gray-300'>Navbar</div>
+  )
+}
+
+export default Navbar
